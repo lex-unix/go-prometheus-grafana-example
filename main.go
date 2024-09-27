016230cee6c33e71608dc85f5fb5a041c6c69a3e
Lex Miin <alexmiyin@gmail.com>
2024-09-27 12:31:14 +0300

    feat: initial commit

---
+++ main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const port int = 8008
+
+type User struct {
+	ID       int    `json:"id"`
+	Username string `json:"username"`
+}
+
+var users []User = []User{
+	{ID: 1, Username: "jane"},
+	{ID: 2, Username: "joe"},
+}
+
+func getUsers(w http.ResponseWriter, r *http.Request) {
+	js, err := json.Marshal(users)
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Internal Server Error"))
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(200)
+	w.Write(js)
+}
+
+func addUser(w http.ResponseWriter, r *http.Request) {
+	var newUser User
+	dec := json.NewDecoder(r.Body)
+	err := dec.Decode(&newUser)
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Internal Server Error"))
+		return
+	}
+	users = append(users, newUser)
+	usersMsg := "updated users:"
+	for _, user := range users {
+		usersMsg += fmt.Sprintf(" %s", user.Username)
+	}
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte(usersMsg))
+}
+
+func main() {
+	http.HandleFunc("GET /users", getUsers)
+	http.HandleFunc("POST /users", addUser)
+
+	log.Println("Server staring on port:", port)
+	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
